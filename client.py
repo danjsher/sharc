e22bcd06746c04c1cb74ef29ff6b95b4f7e3c544
dan <danjsher@gmail.com>
2015-11-18 04:16:59 +0000

    Changed destination IP addresses temporarily while on home network... Should remember to change these back to the pi's network addresses later.

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('192.168.23.1', 12345)
+server_address = ('192.168.1.19', 12345)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
