8dde5466c49c618697c3705397f3147e24988cfa
dan <danjsher@gmail.com>
2015-11-19 00:47:48 +0000

    testing...

--- client.py
+++ client.py
@@ -3,23 +3,8 @@ import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('192.168.23.1', 12345)
+server_address = ('localhost', 12345)
+#server_address = ('192.168.23.1', 12345)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
-try:
-    message = 'This is the message. It will be repeated.'
-    print >>sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
-
-    amount_received = 0
-    amount_expected = len(message)
-
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print >>sys.stderr, 'received "%s"' % data
-
-finally:
-    print >>sys.stderr, 'closing socket'
-    sock.close()
--- server.py
+++ server.py
@@ -4,7 +4,8 @@ import os
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('192.168.1.19', 12345)
+server_address = ('localhost', 12345)
+#server_address = ('192.168.23.1', 12345)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
@@ -13,17 +14,17 @@ sock.listen(1)
 while True:
     print >>sys.stderr, 'waiting for a conneciton'
     (connection, client_address) = sock.accept()
-
-    try:
-        print >>sys.stderr, 'connection from', client_address
-
-        data = connection.recv(1024)
-        parameters = data.split(" ")
-        
-        cmd = 'echo ' + parameters[0] + '=' + parameters[1] + '% > /dev/servoblaster' 
-        print(cmd)
-        os.system(cmd)
+    print >>sys.stderr, 'connection from', client_address
+    
+    while True:
+        try:
+            data = connection.recv(1024)
+            parameters = data.split(" ")
             
-    finally:
-        connection.close()
+            cmd = 'echo ' + parameters[0] + '=' + parameters[1] + '% > /dev/servoblaster' 
+            print(cmd)
+            os.system(cmd)
+            
+        finally:
+            connection.close()
                 
