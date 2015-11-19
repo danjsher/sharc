c8c937f31f573522e20d3a036fac8117f3492f60
dan <danjsher@gmail.com>
2015-11-19 05:38:04 +0000

    bug fix

--- client.py
+++ client.py
@@ -5,7 +5,6 @@ import Queue
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-<<<<<<< HEAD
 server_address = ('localhost', 12345)
 #server_address = ('192.168.23.1', 12345)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
