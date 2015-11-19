a3e9bab9024be330459874c73c8e67dd1ee5027f
dan <danjsher@gmail.com>
2015-11-19 03:07:45 +0000

    messing around with queues

--- client.py
+++ client.py
@@ -1,10 +1,17 @@
 import socket
 import sys
+import threading
+import Queue
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_address = ('localhost', 12345)
 #server_address = ('192.168.23.1', 12345)
 print >>sys.stderr, 'connecting to %s port %s' % server_address
-sock.connect(server_address)
+#sock.connect(server_address)
 
+send_buffer = Queue.Queue()
+
+send_buffer.put(5)
+
+print send_buffer.get()
