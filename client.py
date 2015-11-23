045b01e86175e61aba43f4fc8af663463b899714
dan <danjsher@gmail.com>
2015-11-23 06:28:25 +0000

    Updating client to have cleaner exit upon control C command.

--- client.py
+++ client.py
@@ -16,33 +16,40 @@ def readChannel(channel):
     data = ((adc[1] & 3) << 8) + adc[2]
     return data
 
-host = sys.argv[1]
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = (host, 12345)
-#server_address = ('192.168.23.1', 12345)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
-sock.connect(server_address)
 
-adc_readings = [None, None, None, None, None]
+def runClient(sock):
 
-while True:
-    for i in range(0,5):
-        adc_readings[i] = readChannel(i)
-
-    message = str(adc_readings[0]) + " " + str(adc_readings[1]) + " " + str(adc_readings[2]) + " " + str(adc_readings[3]) + " " + str(adc_readings[4])
-
-    print message
+    adc_readings = [0, 0, 0, 0, 0]
     
-    sock.send(message)
-    sock.recv(1024)
-    time.sleep(.01)
+    while True:
+        for i in range(0,5):
+            adc_readings[i] = readChannel(i)
+            
+            message = str(adc_readings[0]) + " " + \
+                      str(adc_readings[1]) + " " + \
+                      str(adc_readings[2]) + " " + \
+                      str(adc_readings[3]) + " " + \
+                      str(adc_readings[4])
 
-#while True:
-    #message = raw_input("Type your message to send then press enter: ")
-#sock.send(message)
-#sock.recv(1024)
+            print message
+    
+            sock.send(message)
+            sock.recv(1024)
+            time.sleep(.01)
 
-sock.close()
+if __name__ == '__main__':
+    try:
+        host = sys.argv[1]
 
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        server_address = (host, 12345)
+
+        print >>sys.stderr, 'connecting to %s port %s' % server_address
+        sock.connect(server_address)
+        runClient(sock)
+    except KeyboardInterrupt:
+        print 'Closing connection...'
+        sock.close()
